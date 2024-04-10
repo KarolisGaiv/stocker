6c253f7f5cc95248c5c7c67d7d0729d837a3a20a
KarolisG <kgaivenis@gmail.com>
2024-04-10 19:26:45 +0300

    ci: change playwright customer base path

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,18 @@
 import process from 'node:process'
 import { defineConfig, devices } from '@playwright/test'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const formBaseURL = (baseURL: string) => {
+  const repo = process.env.GITHUB_REPOSITORY
+  const basePath = repo ? `/${repo.split('/')[1]}/` : ''
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+  return `${baseURL}${basePath}`
+}
+
 export default defineConfig({
   testDir: './e2e',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
     timeout: 5000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -34,7 +28,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: formBaseURL('http://localhost:5173'),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
