6f88aa8f00c8cb52f66bcb0754b840b71df00bac
KarolisG <kgaivenis@gmail.com>
2024-04-15 15:32:37 +0300

    refactor: fetch sotck price together with stock details

--- src/views/TradeView.vue
+++ src/views/TradeView.vue
@@ -3,16 +3,24 @@ import { ref } from 'vue'
 import { getStockPrice, getStockInformation } from '@/api/stock_api'
 
 const stockName = ref('')
+const stockPrice = ref(0)
 const stockDetails = ref()
 
 async function searchStock() {
+  const price = await getStockPrice(stockName.value.toUpperCase())
   const res = await getStockInformation(stockName.value.toUpperCase())
+
   stockDetails.value = {
     name: res.results.name,
     ticker: res.results.ticker,
     description: res.results.description,
-    homepage: res.results.homepage_url
+    homepage: res.results.homepage_url,
+    logo: res.results.branding.icon_url
   }
+
+  stockPrice.value = price.results[0].o
+
+  stockName.value = ''
 }
 </script>
 
@@ -29,6 +37,10 @@ async function searchStock() {
       <h2>Name: {{ stockDetails.name }}</h2>
       <h3>Ticker: {{ stockDetails.ticker }}</h3>
       <h4>Homepage: {{ stockDetails.homepage }}</h4>
+    </div>
+
+    <div class="stock-price" v-if="stockDetails">
+      <h4>Price: ${{ stockPrice }}</h4>
     </div>
   </main>
 </template>
