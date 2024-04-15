5758cf5530d28c7604bcd5b458dc79466f4764ce
KarolisG <kgaivenis@gmail.com>
2024-04-15 12:03:12 +0300

    test: add test for fetching api data

---
+++ src/api/stock_api.spec.ts
@@ -0,0 +1,27 @@
+import { getStockData } from './stock_api'
+import { describe, expect, vi, it } from 'vitest'
+
+global.fetch = vi.fn()
+
+const MOCK_STOCK = {
+  ticker: 'MCKD',
+  results: [{ o: 123 }],
+  status: 'OK'
+}
+
+describe('getStockData function ', () => {
+  it('fetches stock data successfully', async () => {
+    vi.mocked(fetch).mockResolvedValue({
+      json: () => Promise.resolve(MOCK_STOCK)
+    } as Response)
+
+    const stock = 'MCKD'
+    const stockData = await getStockData(stock)
+
+    expect(stockData).toEqual(MOCK_STOCK)
+
+    expect(fetch).toHaveBeenCalledWith(
+      expect.stringContaining(`https://api.polygon.io/v2/aggs/ticker/${stock}/prev?adjusted=true`)
+    )
+  })
+})
