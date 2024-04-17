5df178c3f54a0c6ccc47934358faa15c861b89e0
KarolisG <kgaivenis@gmail.com>
2024-04-17 10:33:35 +0300

    feat: create register user functionality

---
+++ src/utils/storage.ts
@@ -0,0 +1,25 @@
+interface User {
+  username: string
+  password: string
+}
+
+export const storageService = {
+  getUsers(): User[] {
+    return JSON.parse(localStorage.getItem('users') || '[]')
+  },
+
+  /**
+   * Save a new user to localStorage
+   * @param user The user object to store
+   */
+  saveUser(user: User): void {
+    const users = this.getUsers()
+    users.push(user)
+    localStorage.setItem('users', JSON.stringify(users))
+  },
+
+  doesUserExists(username: string): boolean {
+    const users = this.getUsers()
+    return users.some((user) => user.username === username)
+  }
+}
