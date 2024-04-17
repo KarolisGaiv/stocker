3b649989cc81179d02ba8c62a6d1ff796db9d6aa
KarolisG <kgaivenis@gmail.com>
2024-04-17 11:11:03 +0300

    test: fix init e2e test

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -4,5 +4,5 @@ import { test, expect } from '@playwright/test'
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('./')
-  await expect(page.getByRole('heading', { name: 'This is a test' })).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
 })
