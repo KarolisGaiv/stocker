d55b2eaa3774de5f9fa71c6bcc574eecbcaa96f6
KarolisG <kgaivenis@gmail.com>
2024-04-23 18:50:09 +0300

    test: e2e dashboard with empty user portfolio

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,23 @@
 import { test, expect } from '@playwright/test'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('navigating to dashboard', async ({ page }) => {
   await page.goto('./')
-  await expect(page.getByRole('heading', { name: 'This is A "Home View"' })).toBeVisible()
+  await expect(page.getByRole('banner')).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Stocky App' })).toBeVisible()
+  await expect(page.getByRole('img', { name: 'Stocky App logo' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Profile' })).toBeVisible()
+  await expect(page.getByText('Your Portfolio DetailsCurrent')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Trade' })).toBeVisible()
+})
+
+test('empty user portfolio shows 0 portfolio value and return', async ({ page }) => {
+  await page.goto('./')
+  await expect(page.getByText('Current Value$')).toBeVisible()
+  await expect(page.getByText('Total Return0%')).toBeVisible()
+
+  // current value should be $0
+  await expect(page.getByText('$')).toHaveText('$0')
+
+  // portfolio return should be 0%
+  await expect(page.getByText('%')).toHaveText('0%')
 })
